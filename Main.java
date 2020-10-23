9e220f60759555aaa58b138e295ac5437c14793b
repl.it user <replituser@example.com>
2020-10-23 09:41:51 +0000

    Assignment 1

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello Franziska!");
+    Zahlenfilter ();
   }
+  public static void Zahlenfilter() {
+    for (int i=1; i<201; i++){
+      if((i%5)== 0){
+       System.out.println("ZAHL " +  i +" ist durch 5 teilbar!");
+      }
+      if((i%10)== 9){
+       System.out.println("ZAHL " + i+   " endet auf 9!");
+      }
+      if (((i + (i-1))%3)==0){
+        System.out.println("ZAHL " +i+ " und VORGÄNGER addiert ergeben ERGEBNIS und ERGEBNIS ist durch 3 teilbar");
+      }
+    }
+  }  
 }
